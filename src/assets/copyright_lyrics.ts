51a654a5729b65f8dd47eda84a310a82b0c592ed
Delton Ding <dsh0416@gmail.com>
2019-11-15 15:01:05 +0900

    Add lyrics

--- src/assets/copyright_lyrics.ts
+++ src/assets/copyright_lyrics.ts
@@ -4,66 +4,85 @@ export default {
   '1a': 'Clap your hands now; people clap now',
   '1b': 'Clap your hands y’all, it’s all right',
   '2a': 'Love is a wonderful thing\nI said love \nLove is a wonderful thing\n' +
-        'I said love\nLove is a wonderful thing\nLove is a wonderful thing',
+    'I said love\nLove is a wonderful thing\nLove is a wonderful thing',
   '2b': 'Love is a wonderful thing\nMake ya smile through the pouring rain\n' +
-        'Love is a wonderful thing\nI’ll say it again and again',
+    'Love is a wonderful thing\nI’ll say it again and again',
   '3a': 'What you see is what you get\nAnd boy you ain’t seen nothing yet\n' +
-        'What you see is what you get\nAnd boy you ain’t seen is nothing yet\n',
+    'What you see is what you get\nAnd boy you ain’t seen is nothing yet\n',
   '3b': 'What you see is what you get\nThis is me, hey you\n' +
-        'If you want me, don’t forget\nYou should take me as I am\n' +
-        '‘Cause I can promise you\nBaby, what you see is what you get',
+    'If you want me, don’t forget\nYou should take me as I am\n' +
+    '‘Cause I can promise you\nBaby, what you see is what you get',
   '4a': 'La La La La La La La La La La La La La La La',
   '4b': '[no lyrics; only instrumental]',
   '5a': 'Tiny Bubbles\nIn the Wine\nMake me happy\nMake me feel fine',
   '5b': '[no lyrics; only instrumental]',
   '6a': 'My soul was empty\nMy body broken\nMy heart had no one till you\n' +
-        'You soothed my sorrows,\nAnd through the shadows,\nYou let your light shine through',
+    'You soothed my sorrows,\nAnd through the shadows,\nYou let your light shine through',
   '6b': 'In sleep he sang to me\nIn dreams he came\nThat voice which calls to me\n' +
-        'And speaks my name\nAnd do I dream again?\nFor now I find \nThe Phantom of the Opera is there\n' +
-        'Inside my mind',
+    'And speaks my name\nAnd do I dream again?\nFor now I find \nThe Phantom of the Opera is there\n' +
+    'Inside my mind',
   '7a': 'This one of those love songs\nThat you hear in the middle of the night\n' +
-        'Makes me want to be with you wherever you are tonight\nAnd whoever’s listening just hold your lovers tight\n' +
-        'Cause this is one of those love songs\nOne of those love songs',
+    'Makes me want to be with you wherever you are tonight\nAnd whoever’s listening just hold your lovers tight\n' +
+    'Cause this is one of those love songs\nOne of those love songs',
   '7b': 'Thank God I found you\nI was lost without you\nMy every wish and every dream\n' +
-        'Somehow became reality\nWhen you brought the sunlight\nCompleted my whole life\n' +
-        'I’m overwhelmed with gratitude\n' +
-        '‘Cause baby I’m so thankful I found you\n',
+    'Somehow became reality\nWhen you brought the sunlight\nCompleted my whole life\n' +
+    'I’m overwhelmed with gratitude\n' +
+    '‘Cause baby I’m so thankful I found you\n',
   '8a': 'He’s so fine\nWish he were mine\nThat handsome boy over there\nThe one with the wavy hair\n' +
-        'I don’t know how I’m gonna do it\nBut I’m gonna make him mine\n' +
-        'Be the envy of all those girls\nIt’s just a matter of time',
+    'I don’t know how I’m gonna do it\nBut I’m gonna make him mine\n' +
+    'Be the envy of all those girls\nIt’s just a matter of time',
   '8b': 'My sweet Lord\nMm, my Lord\nMm, my Lord\n\nI really wanna see You\nReally wanna be with You \n' +
-        'Really wanna see You, Lord\nBut it takes so long\nMy lord',
+    'Really wanna see You, Lord\nBut it takes so long\nMy lord',
   '9a': 'Day by Day\nDay by Day\nOh, dear Lord\nThree things I pray\nTo see thee more clearly\n' +
-        'Love thee more dearly\nFollow thee more nearly\nDay by Day',
+    'Love thee more dearly\nFollow thee more nearly\nDay by Day',
   '9b': '[no lyrics; only instrumental]',
   '10a': 'Well it’s time girl that you should know\nWe love now but as soon will go\n' +
-         'And then you will be left alone\nTo find all the answers on your own',
+    'And then you will be left alone\nTo find all the answers on your own',
   '10b': 'I know your eyes in the morning sun\nI feel you touch me in the pouring rain\n' +
-         'And the moment that you wander far from me\nI want to feel you in my arms again',
+    'And the moment that you wander far from me\nI want to feel you in my arms again',
   '11a': 'Whoa thought it was a nightmare\nLord it was so true\nThey told me don’t go walking slow\n' +
-         'The devil’s on the loose\nBetter run through the jungle\nBetter run through the jungle',
+    'The devil’s on the loose\nBetter run through the jungle\nBetter run through the jungle',
   '11b': 'He take the thunder from the mountain\nHe take a lightning from the sky\n' +
-         'He bring a strong man to his begging knee\nHe make the young girl’s mama cry\nYou got to hidey-hide\n' +
-         'You got to jump and run',
+    'He bring a strong man to his begging knee\nHe make the young girl’s mama cry\nYou got to hidey-hide\n' +
+    'You got to jump and run',
   '12a': 'sans toi\nle jour ressemble à la nuit\nla vie ressemble à mort\nce soir écoute-moi.',
   '12b': 'Feelings, nothing more than feelings.Trying to forget my feelings of love.',
   '13a': 'B. B. Bumble and the Stingers, Mott the Hoople, Ray Charles Singers' +
-         'Lonnie Mack and twangin\' Eddy, here\'s my ring we\'re goin\' steady\n' +
-         'Take it easy, take me higher, liar liar, house on fire\n' +
-         'Locomotion, Poco, Passion, Deeper Purple, Satisfaction\n' +
-         'Baby baby gotta gotta gimme gimme gettin\' hotter\n' +
-         'Sammy\'s cookin\', Lesley Gore and Ritchie Valens, end of story\n' +
-          'Mahavishnu, Fujiyama, Kama Sutra, rama-lama\n' +
-         'Richard Perry, Spector, Barry, Rogers-Hart, Nilsson, Harry\n' +
-         'Shimmy shimmy ko-ko bop and Fats is back and Finger Poppin\n',
+    'Lonnie Mack and twangin\' Eddy, here\'s my ring we\'re goin\' steady\n' +
+    'Take it easy, take me higher, liar liar, house on fire\n' +
+    'Locomotion, Poco, Passion, Deeper Purple, Satisfaction\n' +
+    'Baby baby gotta gotta gimme gimme gettin\' hotter\n' +
+    'Sammy\'s cookin\', Lesley Gore and Ritchie Valens, end of story\n' +
+    'Mahavishnu, Fujiyama, Kama Sutra, rama-lama\n' +
+    'Richard Perry, Spector, Barry, Rogers-Hart, Nilsson, Harry\n' +
+    'Shimmy shimmy ko-ko bop and Fats is back and Finger Poppin\n',
   '13b': 'Big Mac, Mc DLT, a Quarter-Pounder with some cheese, Filet-O-Fish, a hamburger, a cheeseburger,' +
-         'a Happy Meal. McNuggets, tasty golden french fries, regular or larger size, and salads: chef salad' +
-         ' or garden, or a chicken salad oriental. Big Big Breakfast, Egg McMuffin, hot hot cakes, and sausage.' +
-         ' Maybe biscuits, bacon, egg and cheese, a sausage, danish, hash browns too.And for dessert' +
-         'hot apple pies, and sundaes three varieties,\n' +
-         'a soft-serve cone, three kinds of shakes, and chocolatey chip cookies.\n' +
-         'And to drink a Coca-Cola, Diet Coke, and orange drink, A Sprite\n' +
-         'and coffee, decaf too, A lowfat milk, also an orange juice.\n' +
-         'I love McDonald\'s, good time great taste,\n' +
-         'and I get this all at one place',
+    'a Happy Meal. McNuggets, tasty golden french fries, regular or larger size, and salads: chef salad' +
+    ' or garden, or a chicken salad oriental. Big Big Breakfast, Egg McMuffin, hot hot cakes, and sausage.' +
+    ' Maybe biscuits, bacon, egg and cheese, a sausage, danish, hash browns too.And for dessert' +
+    'hot apple pies, and sundaes three varieties,\n' +
+    'a soft-serve cone, three kinds of shakes, and chocolatey chip cookies.\n' +
+    'And to drink a Coca-Cola, Diet Coke, and orange drink, A Sprite\n' +
+    'and coffee, decaf too, A lowfat milk, also an orange juice.\n' +
+    'I love McDonald\'s, good time great taste,\n' +
+    'and I get this all at one place',
+  '14a': 'I walk along the street of sorrow, The boulevard of broken dreams, Where gigolo and gigolette,\n' +
+    'Can take a kiss without regret. So they forget their broken dreams.\n' +
+    'You laugh to night and cry tomorrow, When you behold your shattered schemes. And gigolo and gigolette, \n' +
+    'Wake up to find their eyes are wet, With tears that tell of broken dreams.\n',
+  '14b': 'One Rainy Night in Tokyo 濡れた舗道には　ゆれる灯が何故か切なくて',
+  '15a': '校庭の隅にみんなで植えた記念樹　いつの日にか遠いところで思い出すだろう　それはたぶんつらいとき泣きたいとき　緑色の葉っぱ風にゆれる記念樹',
+  '15b': 'どこまでもゆこう　道はきびしくとも　口笛を吹きながら　走ってゆこう',
+  '16a': 'レイニー、レイニーねえレイニーは\n' +
+    '何年経ってても花火を待ってるよ\n' +
+    'レイニー、レイニー、ねえレイニーは 夏を見ているかな',
+  '16b': 'MAKE ME\n' +
+    'MAKE ME\n' +
+    'ねえ　MAKE ME そう\n' +
+    '夢中で走っているよ\n' +
+    '無駄になっていても\n' +
+    'MAKE ME\n' +
+    'MAKE ME\n' +
+    'ねえ　MAKE ME そう\n' +
+    '風になっているから',
 };
