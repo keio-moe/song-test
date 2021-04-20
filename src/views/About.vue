c66685a74dfabc70626ec8a67398bee3ef3b691e
Hideo Daikoku <neildaikoku@gmail.com>
2021-04-20 23:42:54 +0900

    Feature/styling (#18)
    * update styling for about and home
    
    * update india experiment ui
    
    * update labels to match i18n

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,49 @@
 <template lang="pug">
 .about
   h1
-    | About
-  p
-    | How did you find here?
-  p
-    | Made with ❤️️ by Delton
+    | ABOUT
+
+  div 
+  .container
+    | Welcome to the CompMusic Lab for comparative and computational musicology! 
+    | We aim to use science to promote cross-cultural diversity and understanding. 
+    | Our research has appeared in such outlets as 
+    a(target='_blank', noopener, noreferrer, href='https://www.google.com/url?q=https%3A%2F%2Frdcu.be%2FbrZ1Q&sa=D&sntz=1&usg=AFQjCNEBNbH-mtLBlxkLOlAp9aSGFiScfw') Nature
+    | , 
+    a(target='_blank', noopener, noreferrer, href='http://www.google.com/url?q=http%3A%2F%2Fwww.pnas.org%2Fcontent%2F112%2F29%2F8987.full.pdf%3Fwith-ds%3Dyes&sa=D&sntz=1&usg=AFQjCNGGm1ARPcds5gF_0i16C6hSgB2JEA') Proceedings of the National Academy of Sciences
+    | , 
+    a(target='_blank', noopener, noreferrer, href='https://www.google.com/url?q=https%3A%2F%2Fwww.cambridge.org%2Fcore%2Fjournals%2Fbehavioral-and-brain-sciences%2Farticle%2Fmusic-as-a-coevolved-system-for-social-bonding%2FF1ACB3586FD3DD5965E56021F506BC4F&sa=D&sntz=1&usg=AFQjCNGVVI6E3oWXV6SRWIy6ImdRrlhkKA') Behavioral and Brain Sciences
+    |, and 
+    a(target='_blank', noopener, noreferrer, href='https://www.google.com/url?q=https%3A%2F%2Fwww.researchgate.net%2Fpublication%2F271698023_Mapping_Music_Cluster_Analysis_Of_Song-Type_Frequencies_Within_And_Between_Cultures&sa=D&sntz=1&usg=AFQjCNGvHrSpn6nQirxiU2R6oD7zKT_FaQ') Ethnomusicology
+    | , and has been covered by media including 
+    a(target='_blank', noopener, noreferrer, href='https://www.google.com/url?q=https%3A%2F%2Fwww.discovermagazine.com%2Fplanet-earth%2Fwhere-did-music-come-from-here-are-the-leading-theories&sa=D&sntz=1&usg=AFQjCNHhJgFMHLyerEZvw7dFkhKldMZBwA') Discover Magazine 
+    |,
+    a(target='_blank', noopener, noreferrer, href='http://www.google.com/url?q=http%3A%2F%2Fwww.npr.org%2Fsections%2Fgoatsandsoda%2F2015%2F07%2F09%2F418587373%2Ffrom-pygmy-hunting-songs-to-taylor-swift-what-makes-music-universal&sa=D&sntz=1&usg=AFQjCNE1xgswSJLA0Lnhr-PWY0sabc4uEw') NPR 
+    |, 
+    a(target='_blank', noopener, noreferrer, href='https://www.google.com/url?q=https%3A%2F%2Fwww.nytimes.com%2Finteractive%2F2018%2F01%2F25%2Farts%2Fmusic%2Fhistory-of-song.html&sa=D&sntz=1&usg=AFQjCNE_VKSEa9gHn51gvMlqtBdfNd6peA') the New York Times  
+    |,
+    a(target='_blank', noopener, noreferrer, href='https://www.google.com/url?q=https%3A%2F%2Fwww.billboard.com%2Farticles%2Fbusiness%2Flegal-and-management%2F8547957%2Fmusicologists-katy-perry-dark-horse&sa=D&sntz=1&usg=AFQjCNFo8iO5hVaBBifkP3PLFO8O0UnjSA') Billboard
+    |, and 
+    a(target='_blank', noopener, noreferrer, href='https://www.google.com/url?q=https%3A%2F%2Fwww.nbcnews.com%2Fsciencemain%2Ftake-note-folk-music-could-track-human-migrations-2D11591384&sa=D&sntz=1&usg=AFQjCNEsR-yTEsD6XSYExOqmyUe3hR8TsA') NBC News
+    |. 
+    | You can see some examples of who we are and what we do on this site.
+  small
+    | Made with ❤️️ by Delton Ding and Hideo Daikoku 
+    br
+    | &copy; Comp Music Lab 2021
 </template>
 
 <style lang="stylus" scoped>
 .about
-  text-align center
+    text-align center
+  h1
+    font-size 5rem
+    margin-bottom 1rem
+  .container
+    margin 2rem auto
+    text-align left
+    max-width 600px
+    a
+      text-decoration none
+      color red
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 .home
   .routes
     a.route(v-for="route in routes", :href="route.src")
-      | {{ $t(route.title) }}
+      | {{ $t(route.label) }}
 </template>
 
 <script lang="ts">
@@ -10,6 +10,7 @@ import { Component, Vue } from 'vue-property-decorator';
 
 interface RouteDefinition {
   title: string;
+  label: string;
   src: string;
 }
 
@@ -19,7 +20,13 @@ interface RouteDefinition {
 export default class Home extends Vue {
   private routes: RouteDefinition[] = [
     {
+      title: 'indianMusic',
+      label: 'Indian Music Experiment',
+      src: '/experiments/indian_music_evaluation',
+    },
+    {
       title: 'aesthetics',
+      label: 'Aesthetics Experiment',
       src: '/experiments/aesthetics',
     },
     // {
@@ -28,10 +35,12 @@ export default class Home extends Vue {
     // },
     {
       title: 'copyrightFull',
+      label: 'Full Copyright',
       src: '/experiments/copyright/full',
     },
     {
       title: 'copyrightNewWorkshop',
+      label: 'New Copyright Workshop',
       src: '/experiments/copyright/new_workshop',
     },
     // {
@@ -40,6 +49,7 @@ export default class Home extends Vue {
     // },
     {
       title: 'worldMusicWorkshop',
+      label: 'World Music Workshop',
       src: '/experiments/worldmusic/workshop/evaluation',
     },
     // {
@@ -54,20 +64,31 @@ export default class Home extends Vue {
     //   title: 'worldMusicYamaha',
     //   src: '/experiments/worldmusic/yamaha/evaluation',
     // },
-    {
-      title: 'indianMusic',
-      src: '/experiments/indian_music_evaluation',
-    },
   ];
 }
 </script>
 
 <style lang="stylus" scoped>
 .routes
-  text-align center
+  display flex
+  flex-direciton row
+  min-width 400px
+  max-width 800px
+  margin 0 auto
+  flex-wrap wrap
   a.route
-    display block
-    margin 40px 0
+    text-transform uppercase
+    width 40%
     font-size 24px
+    color white
+    text-decoration none
+    padding 3rem 1rem
+    background-color #42b983
+    border-radius 5px
+    border 1px solid #42b983
+    height 10rem
+    margin 5px
+  a.route:hover
+    background-color white
     color #42b983
 </style>
