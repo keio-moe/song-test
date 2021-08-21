1a9a07313803dda88286acfa40373446802390aa
hideo <neildaikoku@gmail.com>
2021-08-21 09:10:34 +0900

    add consent form

---
+++ src/assets/archive/old_routes.js
@@ -0,0 +1,40 @@
+// {
+//   title: 'indianMusic',
+//   label: 'Start Indian Music Experiment',
+//   src: '/experiments/indian_music_evaluation',
+// },
+// {
+//   title: 'aesthetics',
+//   label: 'Aesthetics Experiment',
+//   src: '/experiments/aesthetics',
+// },
+// {
+//   title: 'copyrightWorkshop',
+//   src: '/experiments/copyright/workshop',
+// },
+// {
+//   title: 'copyrightFull',
+//   label: 'Full Copyright',
+//   src: '/experiments/copyright/full',
+// },
+// {
+//   title: 'copyrightNewWorkshop',
+//   label: 'New Copyright Workshop',
+//   src: '/experiments/copyright/new_workshop',
+// },
+// {
+//   title: 'worldMusic',
+//   src: '/experiments/worldmusic/full/evaluation',
+// },
+// {
+//   title: 'worldMusicWorkshopRank',
+//   src: '/experiments/worldmusic/workshop/rank',
+// },
+// {
+//   title: 'worldMusicWorkshopSimilarity',
+//   src: '/experiments/worldmusic/workshop/similarity',
+// },
+// {
+//   title: 'worldMusicYamaha',
+//   src: '/experiments/worldmusic/yamaha/evaluation',
+// },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
-<template lang="pug">
+<template lang='pug'>
 .home
+  .pdf
+    iframe.file(src='/Experiment Consent Form.pdf')
+  .v-checkbox
+    input(type='checkbox' v-on:click='setActive(0)')
+    .v-label I agree to the terms and conditions of mentioned above.
   .routes
-    a.route(v-for="route in routes", :href="route.src")
+    a.route(v-for='route in routes', :href='route.src' v-if='route.active==true')
       | {{ $t(route.label) }}
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import { Component, Vue } from 'vue-property-decorator';
 
 interface RouteDefinition {
   title: string;
   label: string;
   src: string;
+  active: boolean;
 }
 
 @Component({
@@ -23,76 +29,65 @@ export default class Home extends Vue {
       title: 'worldMusicWorkshop',
       label: 'Start World Music Workshop',
       src: '/experiments/worldmusic/workshop/evaluation',
+      active: false,
     },
-    {
-      title: 'indianMusic',
-      label: 'Start Indian Music Experiment',
-      src: '/experiments/indian_music_evaluation',
-    },
-    // {
-    //   title: 'aesthetics',
-    //   label: 'Aesthetics Experiment',
-    //   src: '/experiments/aesthetics',
-    // },
-    // {
-    //   title: 'copyrightWorkshop',
-    //   src: '/experiments/copyright/workshop',
-    // },
-    // {
-    //   title: 'copyrightFull',
-    //   label: 'Full Copyright',
-    //   src: '/experiments/copyright/full',
-    // },
-    // {
-    //   title: 'copyrightNewWorkshop',
-    //   label: 'New Copyright Workshop',
-    //   src: '/experiments/copyright/new_workshop',
-    // },
-    // {
-    //   title: 'worldMusic',
-    //   src: '/experiments/worldmusic/full/evaluation',
-    // },
-    // {
-    //   title: 'worldMusicWorkshopRank',
-    //   src: '/experiments/worldmusic/workshop/rank',
-    // },
-    // {
-    //   title: 'worldMusicWorkshopSimilarity',
-    //   src: '/experiments/worldmusic/workshop/similarity',
-    // },
-    // {
-    //   title: 'worldMusicYamaha',
-    //   src: '/experiments/worldmusic/yamaha/evaluation',
-    // },
   ];
+  private setActive(route: number) {
+    this.routes[route].active = !this.routes[route].active;
+  }
 }
 </script>
 
-<style lang="stylus" scoped>
-.routes
-  display flex
-  flex-direction row
-  min-width 400px
-  max-width 800px
-  margin 0 auto
-  flex-wrap wrap
-  a.route
-    text-transform uppercase
-    width 40%
-    font-size 24px
-    color white
-    text-decoration none
-    padding 3rem 1rem
-    background-color #42b983
-    border-radius 5px
-    border 1px solid #42b983
-    height 10rem
-    margin 5px
-  a.route:hover
-    background-color white
-    color #42b983
-  @media screen and (max-width: 600px)
-    width 90vw
-    a.route
-      width 100%
+<style lang='stylus' scoped>
+.pdf {
+  margin: 2rem auto;
+  width: 90vw;
+  height: 70vh;
+
+  iframe.file {
+    width: 100%;
+    height: 100%;
+  }
+}
+
+.v-checkbox{
+  display: flex;
+  margin: 0 auto 2rem auto;
+  justify-content center;
+}
+
+.routes {
+  display: flex;
+  flex-direction: row;
+  min-width: 400px;
+  max-width: 800px;
+  margin: 0 auto;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  a.route {
+    text-transform: uppercase;
+    font-size: 24px;
+    color: white;
+    text-decoration: none;
+    padding: 1rem;
+    background-color: #42b983;
+    border-radius: 5px;
+    border: 1px solid #42b983;
+    margin: 5px;
+  }
+
+  a.route:hover {
+    background-color: white;
+    color: #42b983;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 90vw;
+
+    a.route {
+      width: 100%;
+    }
+  }
+}
 </style>
